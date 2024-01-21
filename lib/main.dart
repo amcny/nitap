8ed93a2bb04f373b2b537e8d21976aa39f26eb06
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-21 16:22:59 +0000

    add action

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ void main() async {
   // Start initial custom actions code
   await actions.lockOrientation();
   await actions.inAppUpdate();
+  await actions.setRefreshRate();
   // End initial custom actions code
 
   await FlutterFlowTheme.initialize();
